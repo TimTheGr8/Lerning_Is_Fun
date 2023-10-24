fa5bc21e14484cb96f6543ba2078a8854c839cf4
TimTheGr8 <tjanssen4@gmail.com>
2023-10-23 21:42:48 -0500

    Added the functionality of the Settings Screen. Also adjusted the Title Screen to look and feel better.

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Slider _masterVolumeSlider;
+    [SerializeField]
+    private Slider _soundAudioSlider;
+    [SerializeField]
+    private Slider _brightnessSlider;
+    [SerializeField]
+    private Image _overlayImage;
+    [SerializeField]
+    private AudioMixer _mixer;
+
+    public void AdjustBrightness()
     {
-        
+        var tempColor = _overlayImage.color;
+        tempColor.a = _brightnessSlider.value;
+        _overlayImage.color = tempColor;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AdjustMasterVolume()
     {
-        
+        _mixer.SetFloat("MasterAudio", _masterVolumeSlider.value);
+    }
+
+    public void AdjustSoundVolume()
+    {
+        _mixer.SetFloat("SoundAudio", _soundAudioSlider.value);
     }
 }
