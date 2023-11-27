83c43e97c54f518b9c64fed27181075907d2c226
Timothy Janssen <tjanssen4@gmail.com>
2023-11-27 16:57:03 -0600

    Started working on correctly shuffling the list to get non duplicate names.

--- Assets/Scripts/States/States.cs
+++ Assets/Scripts/States/States.cs
@@ -30,6 +30,8 @@ public class States : MonoBehaviour
     private List<TMP_Text> _stateNames = new List<TMP_Text>();
     [SerializeField]
     private List<Transform> _stateNameStartingPosition = new List<Transform>();
+    [SerializeField]
+    private List<StatesSO> _tempList = new List<StatesSO>();
     
     private List<StatesSO> _currentSateRegion = new List<StatesSO>();
     private string _currentRegionName = "";
@@ -66,7 +68,7 @@ public class States : MonoBehaviour
         }
         
         Shuffle(_currentSateRegion);
-        SetCurrentState();
+        SetStateNames();
     }
 
     public void Shuffle(List<StatesSO> states)
@@ -82,47 +84,34 @@ public class States : MonoBehaviour
         }
     }
 
-    public void Shuffle(List<string> states)
-    {
-        var count = states.Count;
-        var last = count - 1;
-        for (var i = 0; i < last; ++i)
-        {
-            var r = Random.Range(i, count);
-            var tmp = states[i];
-            states[i] = states[r];
-            states[r] = tmp;
-        }
-    }
+    //public void Shuffle(List<string> states)
+    //{
+    //    var count = states.Count;
+    //    var last = count - 1;
+    //    for (var i = 0; i < last; ++i)
+    //    {
+    //        var r = Random.Range(i, count);
+    //        var tmp = states[i];
+    //        states[i] = states[r];
+    //        states[r] = tmp;
+    //    }
+    //}
 
     private void SetCurrentState()
     {
-        List<string> stateNamesHolder = new List<string>();
-        var testName = "";
-        _currentStateSprite.sprite = _currentSateRegion[_currentIndex].GetStateSprite();
-        stateNamesHolder.Add(_currentSateRegion[_currentIndex].GetStateName());
-        //_stateNames[0].text = _currentSateRegion[_currentIndex].GetStateName();
-        for (int i = 1; i < 4; i++)
-        { 
-            var rand = Random.Range(0, _currentSateRegion.Count);
-            for (int j = 0; j < _stateNames.Count; j++)
-            {
-                testName = _currentSateRegion[rand].GetStateName();
-                if (_stateNames[j] == _stateNames[i] || _stateNames[0] == _stateNames[j])
-                {
-                    rand = Random.Range(0, _currentSateRegion.Count);
-                    testName = _currentSateRegion[rand].GetStateName();
-                    continue;
-                }
-            }
-            stateNamesHolder.Add(testName);
-            //_stateNames[i].text = testName;
-        }
-        Shuffle(stateNamesHolder);
+        
+    }
+
+    public void SetStateNames()
+    {
+        _tempList = _currentSateRegion;
+        Shuffle(_tempList);
+        // Assign Random names to the answers in the game.
         for (int i = 0; i < _stateNames.Count; i++)
         {
-            _stateNames[i].text = stateNamesHolder[i];
+            _stateNames[i].text = _tempList[i].GetStateName();
         }
+
     }
 
     public string GetCurrentState()
